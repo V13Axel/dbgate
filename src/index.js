e0fb6b9e7c0fdfa96ffff440748917537e5a2c0e
V13Axel <alex@stechstudio.com>
2024-03-26 12:10:36 -0400

    All four palettes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
+import catppuccin from './catppuccin-vars.css';
 import css from './theme.css';
 
-const mocha = {
-  themeName: 'Catpuccin Mocha',
-  themeType: 'dark',
-  themeClassName: 'theme-dbgate-plugin-catppuccin-mocha',
-  themeCss: css,
-};
-
 export default {
-    themes: [
-      mocha,
+  themes: [
+    {
+      themeName: 'Catpuccin Mocha',
+      themeType: 'dark',
+      themeClassName: 'theme-dbgate-plugin-catppuccin-mocha',
+      themeCss: css,
+    },
+    {
+      themeName: 'Catpuccin Macchiato',
+      themeType: 'dark',
+      themeClassName: 'theme-dbgate-plugin-catppuccin-macchiato',
+      themeCss: css,
+    },
+    {
+      themeName: 'Catpuccin Frappe',
+      themeType: 'dark',
+      themeClassName: 'theme-dbgate-plugin-catppuccin-frappe',
+      themeCss: css,
+    },
+    {
+      themeName: 'Catpuccin Latte',
+      themeType: 'dark',
+      themeClassName: 'theme-dbgate-plugin-catppuccin-latte',
+      themeCss: css,
+    },
   ],
 };
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
+  mode: 'production',
   module: {
     rules: [
       {
